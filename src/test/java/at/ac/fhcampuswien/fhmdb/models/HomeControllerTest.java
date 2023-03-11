fc92f459dcd60348bef28fac16cc678d8512701e
ibrakel <113463426+ibrakel@users.noreply.github.com>
2023-03-11 13:49:47 +0100

    Move and refactor test units

--- src/test/java/at/ac/fhcampuswien/fhmdb/models/HomeControllerTest.java
+++ src/test/java/at/ac/fhcampuswien/fhmdb/models/HomeControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HomeControllerTest {
@@ -17,4 +18,56 @@ public class HomeControllerTest {
 
         assertTrue(instance.filterByString(movie1, ""));
     }
+
+    @Test
+    public void FilterByGenre_match_single_genre() {
+        Movie movie1 = new Movie("Movie3", "", Collections.singletonList("Comedy"));
+
+
+        String genreToFilter = "Comedy";
+        HomeController instance = new HomeController();
+        assertTrue(instance.filterByGenre(movie1, genreToFilter));
+    }
+    @Test
+    public void FilterByGenre_no_match_single_genre() {
+        Movie movie1 = new Movie("Movie4", "", Collections.singletonList("Action"));
+
+        String genreToFilter = "Comedy";
+        HomeController instance = new HomeController();
+        assertFalse(instance.filterByGenre(movie1, genreToFilter));
+    }
+
+    @Test
+    public void FilterByGenre_match_double_genre() {
+        Movie movie1 = new Movie("Movie1","", Collections.singletonList("Drama, Comedy"));
+
+        String genreToFilter = "Comedy";
+        HomeController instance = new HomeController();
+        assertTrue(instance.filterByGenre(movie1, genreToFilter));
+    }@Test
+    public void FilterByGenre_no_match_double_genre() {
+        Movie movie1 = new Movie("Movie2", "", Collections.singletonList("Action, Thriller"));
+
+        String genreToFilter = "Drama";
+        HomeController instance = new HomeController();
+
+        assertFalse(instance.filterByGenre(movie1, genreToFilter));
+    }
+    @Test
+    public void FilterByGenre_Null() {
+        Movie movie1 = new Movie("Movie1","", Collections.singletonList("Drama, Comedy"));
+
+        HomeController instance = new HomeController();
+
+        assertTrue(instance.filterByGenre(movie1, null));
+    }
+    /*
+    @Test
+    public void Filter_input_cleanup() {
+
+        HomeController instance = new HomeController();
+        String input = "Dr"'? %$§&><am+*!°^@;:a";
+
+        assertEquals(instance.cleanString(input), "Drama"));
+    }*/
 }
